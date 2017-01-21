9b0859b2df81299d4bb34a6890c8e7c5ece1adcc
Nacho <iterriza@ucm.es>
2017-01-21 13:22:12 +0100

    pipes

--- lab4/SynchronousIOMultiplexing/ej1/ej1.c
+++ lab4/SynchronousIOMultiplexing/ej1/ej1.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <sys/types.h>
+#include <sys/select.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 
@@ -26,6 +27,7 @@ int main() {
 
     if(pipe == -1) {
         perror("Can't open FIFO");
+        close(pipe);
         return -1;
     }
 
@@ -33,8 +35,79 @@ int main() {
 
     if(pipe2 == -1) {
         perror("Can't open FIFO");
+        close(pipe);
+        close(pipe2);
         return -1;
     }
+    
+    int sel, current;
 
+    do {
 
+        fd_set pipes;
 
+        FD_ZERO(&pipes);
+        FD_SET(pipe, &pipes);
+        FD_SET(pipe2, &pipes);
+
+        int max, nump;
+
+        if(pipe < pipe2) 
+            max = pipe2 + 1;
+        else
+            max = pipe + 1;
+
+        sel = select(max, &pipes, NULL, NULL, NULL);
+
+        if(sel > 0) {
+
+            if(FD_ISSET(pipe, &pipes))
+                current = pipe;
+            else if(FD_ISSET(pipe2, &pipes))
+                current = pipe2;
+
+            ssize_t toread = BUFFER_LENGTH;
+            
+            while(toread == BUFFER_LENGTH) {
+
+                toread = read(current, buffer, BUFFER_LENGTH);
+
+                if(toread == -1) {
+                    perror("Can't read from FIFO");
+                    close(pipe);
+                    close(pipe2);
+                    return -1;
+                }
+
+                buffer[toread] = '\0';
+                printf("Pipe number %i contains: %s\n", (current==pipe) ? 1 : 2, buffer);
+            }
+
+            if(current == pipe) {
+                close(pipe);
+                if((pipe = open("pipe", O_RDONLY | O_NONBLOCK)) == -1) {
+                        perror("Can't open pipe");
+                        close(pipe);
+                        close(pipe2);
+                        return -1;
+                }
+            } else if(current == pipe2) {
+                close(pipe2);
+                if((pipe2 = open("pipe2", O_RDONLY | O_NONBLOCK)) == -1) {
+                        perror("Can't open pipe");
+                        close(pipe);
+                        close(pipe2);
+                        return -1;
+                }
+            }
+                
+
+        }
+    } while(sel != -1);
+
+    perror("Error in selection");
+
+    close(pipe);
+    close(pipe2);
+    return -1;
+}
